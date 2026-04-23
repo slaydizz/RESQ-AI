738ec33519799a806730230eb885d7be69f2d145
slaydizz <106423892+slaydizz@users.noreply.github.com>
2026-04-23 16:32:35 +0530

    Update gemini.js

--- src/gemini.js
+++ src/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyAy7ChuwwnCquI5USozlzNZS-5jA1IHzX8");
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
 export async function analyzeReport(imagePart) {
   try {
